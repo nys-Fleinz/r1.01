48c818d4328056567fbdef4f9e1e82fb81014a96
nys-Fleinz <ethan.seulin@gmail.com>
2024-11-25 20:39:55 +0100

    la vrai fin :D

--- tp11/GestionParcVehicules/parcAVehicules.java
+++ tp11/GestionParcVehicules/parcAVehicules.java
@@ -15,7 +15,9 @@ class parcAVehicules extends Program {
     String toString(Parc parking) {
         String liste="";
         for(int i=0; i<parking.nbVehicules; i=i+1) {
-            liste=liste+parking.vehicules[i].modele+" "+parking.vehicules[i].annee+"\n";
+            if(!equals(parking.vehicules[i].modele,"")) {
+                liste=liste+parking.vehicules[i].modele+" "+parking.vehicules[i].annee+"\n";
+            }
         }
         return liste;
     }
@@ -52,8 +54,70 @@ class parcAVehicules extends Program {
     }
 
     int nbVoitures(Parc p, String modele) {
+        int nombreDeVoitures=0;
         for(int i=0; i<p.nbVehicules; i=i+1) {
-            
+            if(equals(p.vehicules[i].modele,modele)) {
+                nombreDeVoitures=nombreDeVoitures+1;
+            }
+        }
+        return nombreDeVoitures;
+    }
+
+    boolean fusionParcs(Parc p1, Parc p2) {
+        int compteur=p2.nbVehicules-1;
+        boolean fusion=false;
+        if(p1.nbVehicules<NB_MAX) {
+            fusion=true;
+        }
+        while(p1.nbVehicules<NB_MAX && compteur>=0) {
+            ajouterVehicule(p1, p2.vehicules[compteur]);
+            p2.vehicules[compteur] = newVehicule("", 0);
+            p2.nbVehicules=p2.nbVehicules-1;
+            compteur=compteur-1;
+        }
+        return fusion;
+    }
+
+    void testFusionParc() {
+        Parc parc1 = newParcVide();
+        ajouterVehicule(parc1, newVehicule("Yaris", 2020));
+        ajouterVehicule(parc1, newVehicule("Yaris", 2020));
+        ajouterVehicule(parc1, newVehicule("Yaris", 2020));
+        Parc parc2 = newParcVide();
+        ajouterVehicule(parc2, newVehicule("Yaris", 2020));
+        ajouterVehicule(parc2, newVehicule("Yaris", 2020));
+        ajouterVehicule(parc2, newVehicule("Yaris", 2020));
+        fusionParcs(parc1, parc2);
+        println("\nParc 1:\n"+toString(parc1) +"----");
+        println("\nParc 2:\n"+toString(parc2) +"----");
+    }
+
+    void suppressionVehicule(Parc p, Vehicule v) {
+        int i=0;
+        boolean supprime=false;
+        while(i<p.nbVehicules && !supprime) {
+            if(equals(p.vehicules[i].modele,v.modele) && p.vehicules[i].annee==v.annee) {
+                for(int j=p.nbVehicules-1; j>i; i=i+1) {
+                    p.vehicules[i]=p.vehicules[i+1];
+                }
+                p.vehicules[i] = newVehicule("", 0);
+            } else {
+                i=i+1;
+            }
         }
     }
+
+
+    void testSupressionVehicules() {
+        Parc parc1 = newParcVide();
+        ajouterVehicule(parc1, newVehicule("Yaris", 2020));
+        ajouterVehicule(parc1, newVehicule("Renault", 2020));
+        ajouterVehicule(parc1, newVehicule("Yaris", 2020));
+        ajouterVehicule(parc1, newVehicule("Yaris", 2020));
+        ajouterVehicule(parc1, newVehicule("Yaris", 2020));
+        ajouterVehicule(parc1, newVehicule("Renault", 2020));
+        suppressionVehicule(parc1, newVehicule("Renault", 2020));
+        println(toString(parc1));
+    }
+
 }
